cf471f3c832a1ebc50e0e1d42084c22128bdc761
Gwyneth Semanisin <gesemanisin@gmail.com>
2019-06-04 13:11:52 -0500

    Initial Commit

---
+++ DivvyBike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DivvyBike
+//
+//  Created by Gwyneth Semanisin on 6/4/19.
+//  Copyright © 2019 Gwyneth Semanisin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
